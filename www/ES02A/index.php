842906fa563e9a3a7063fa89d546d4c281d7f58c
Andrea-Bagli <bagliandrea618@gmail.com>
2024-10-03 07:59:57 +0000

    commit

---
+++ www/ES02A/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Pagina di login</title>
+</head>
+<body>
+  <h3>Accesso a pagina riservata</h3>
+  
+  <form action="login.php" method="post">
+    
+    <label for="username"><b>Username</b></label>
+    <input type="text" name="nomeutente" /><br />
+
+    <label for="password"><b>Password</b></label>
+    <input type="password" name="password" /><br />
+    
+    <input name="submit" type="submit" value="Invia" />
+  
+  </form>
+
+</body>
+</html>
