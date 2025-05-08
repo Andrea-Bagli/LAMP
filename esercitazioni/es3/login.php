2957bb9908017d77e9c70482586dc4157d8865b2
Andrea-Bagli <bagliandrea618@gmail.com>
2025-05-08 06:57:52 +0000

    commit

--- esercitazioni/es3/login.php
+++ esercitazioni/es3/login.php
@@ -1,16 +1,34 @@
 <?php
-// Credenziali valide
-$username = "mario";
-$password = "scuola123";
+// Connessione al database
+$connessione = mysqli_connect("localhost", "root", "", "scuola");
 
-// Dati inviati dal form
-$utente = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
-
-// Verifica credenziali
-if ($utente === $username && $password === $password) {
-    echo "Login riuscito! Benvenuto.";
-} else {
-    echo "Username o password errati.";
+// Controllo se la connessione funziona
+if (!$connessione) {
+    die("Connessione fallita: " . mysqli_connect_error());
 }
+
+// Prendo i dati inviati dal form
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Controllo se l'utente esiste
+$query = "SELECT * FROM utenti WHERE username = '$username' AND password = '$password'";
+$risultato = mysqli_query($connessione, $query);
+
+// Se l'utente esiste
+if (mysqli_num_rows($risultato) > 0) {
+    echo "Login riuscito! Benvenuto, " . $username;
+} else {
+    // L'utente non esiste, lo aggiungo
+    $inserimento = "INSERT INTO utenti (username, password) VALUES ('$username', '$password')";
+    
+    if (mysqli_query($connessione, $inserimento)) {
+        echo "Utente registrato. Accesso riuscito! Benvenuto, " . $username;
+    } else {
+        echo "Errore nella registrazione.";
+    }
+}
+
+// Chiudo la connessione
+mysqli_close($connessione);
 ?>
